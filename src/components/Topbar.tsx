eff45acd04dfa127c6cc5489d3ed5c9781501905
Nestor Mosquera <yunsde182gmail.com>
2022-09-19 11:58:03 -0500

    update

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -3,7 +3,7 @@ import styles from '@styles/Topbar.module.css'
 const Topbar = () => {
     return (
     <div className={styles.containerTopBar}>
-        <p>Start now and save 50% form 3 months </p>
+        <p>Empieza ahora y ahorra un 50% por 3 meses</p>
     </div>
     )
 }
--- src/sections/home/banner/index.tsx
+++ src/sections/home/banner/index.tsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import styles from './Banner.module.css'
+import Image from "next/image";
+import React from "react";
+import styles from "./Banner.module.css";
 
 const Banner = () => {
   return (
     <div className={styles.container}>
-        <div>
-
-      <h2>El software inteligente de gestion para negocios</h2>
-      <p>Helebba es la herramienta en la nube que tiene todo lo que necesitas para gestionar tu empresa donde quieras y cuando quieras.</p>
       <div>
-        <button>Empieza gratis</button>
+        <h2>El software inteligente de gestion para negocios</h2>
+        <p>
+          Helebba es la herramienta en la nube que tiene todo lo que necesitas
+          para gestionar tu empresa donde quieras y cuando quieras.
+        </p>
+        <div>
+          <button>Empieza gratis</button>
+        </div>
       </div>
-      </div>
-      
-    </div>
-  )
-}
 
-export default Banner
+      <Image src="/img/banner.jpg" width={500} height={500} />
+    </div>
+  );
+};
+
+export default Banner;
